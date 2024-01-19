2bab90b8b49dc8f35a8d999ccbf4c4abacdf4731
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 14:32:04 +0700

    update client.py

--- client.py
+++ client.py
@@ -6,12 +6,16 @@ import os, sys
 chat = sys.argv[1]
 url = sys.argv[2]
 session_string = sys.argv[3]
+as_chat = sys.argv[4]
 
 
 client = Client("telestream", session_string=session_string)
 app = PyTgCalls(client)
-
 app.start()
-app.join_group_call(chat, MediaStream(url,))
+if as_chat:
+    peer = client.resolve_peer(as_chat)
+    app.join_group_call(chat, MediaStream(url,), as_chat=peer)
+else:
+    app.join_group_call(chat, MediaStream(url,))
 os.system("echo Đã kết nối")
 idle()
