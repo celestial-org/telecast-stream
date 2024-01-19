009e9550a83b995207955d06ac9605635f4c2436
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 20:27:31 +0700

    update main.py

--- main.py
+++ main.py
@@ -13,12 +13,10 @@ def executor(chat, url):
 def telestream__():
     chat = request.args.get("chat")
     url = request.args.get("url")
-    session = request.args.get("session")
-    as_chat = request.args.get("as")
     if not chat:
         return "Cần tham số chat=(username hoặc id của nhóm, channel)"
     if not url:
         return "Cần tham số url=(Liên kết video/âm thanh để phát trực tiếp)"
     if not session:
         return "Thiếu session=(session string)"
-    return Response(executor(chat, url, session, as_chat), content_type="text/plain")
+    return Response(executor(chat, url), content_type="text/plain")
