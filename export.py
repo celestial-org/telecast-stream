3e4859ff91044b7bf9bf577f9357b19a4c26eab1
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 11:54:22 +0700

    update export.py

--- export.py
+++ export.py
@@ -3,7 +3,7 @@ import os
 
 api_id = input("API ID: ")
 api_hash = input("API HASH: ")
-client = Client("telestream", api_id, api_hash, in_memory=True, takeout=True, device_model="telestream", system_version="Ubuntu", app_version="1.0")
+client = Client("telestream", api_id, api_hash, in_memory=True, device_model="telestream", system_version="Ubuntu", app_version="1.0")
 
 with client:
     session_string = client.export_session_string()
