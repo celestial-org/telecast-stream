0a444182e0400825fee1f578a28de749e24dffb2
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 20:24:34 +0700

    update client.py

--- client.py
+++ client.py
@@ -5,11 +5,11 @@ import os, sys
 
 chat = sys.argv[1]
 url = sys.argv[2]
-session_string = sys.argv[3]
-as_chat = sys.argv[4]
+session_string = os.getenv("SS")
+as_chat = os.getenv("AS")
+chat = os.getenv("CHAT")
 
-
-client = Client("telestream", session_string=session_string)
+client = Client("telecast", session_string=session_string)
 app = PyTgCalls(client)
 app.start()
 if as_chat:
