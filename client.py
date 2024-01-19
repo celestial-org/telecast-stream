162cb550e0ac730c6d569f0c130283bcebaab5f4
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 11:30:48 +0700

    update client.py

---
+++ client.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import MediaStream
+import os, sys
+
+chat = sys.argv[1]
+url = sys.argv[2]
+
+session_string = os.getenv("SESSION")
+client = Client("telestream", session_string=session_string)
+app = PyTgCalls(client)
+
+app.start()
+app.join_group_call(chat, MediaStream(url,))
+os.system("echo Đã kết nối")
+idle()
