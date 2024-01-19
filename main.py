4a1e2c58132b3d266dc249416c105cb1b8c78ff9
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 14:27:42 +0700

    update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import subprocess
 
 app = Flask("__telestream__")
 
-def executor(chat, url, session):
-    process = subprocess.Popen(["python3", "-u", "client.py", f"{chat}", f"{url}", f"{session}"], stdout=subprocess.PIPE, universal_newlines=True)
+def executor(chat, url, session, as_chat):
+    process = subprocess.Popen(["python3", "-u", "client.py", f"{chat}", f"{url}", f"{session}", f"{as_chat}"], stdout=subprocess.PIPE, universal_newlines=True)
     for line in process.stdout:
         yield line + "\n"
     process.wait()
@@ -14,10 +14,13 @@ def telestream__():
     chat = request.args.get("chat")
     url = request.args.get("url")
     session = request.args.get("session")
+    as_chat = request.args.get("as")
     if not chat:
         return "Cần tham số chat=(username hoặc id của nhóm, channel)"
     if not url:
         return "Cần tham số url=(Liên kết video/âm thanh để phát trực tiếp)"
     if not session:
         return "Thiếu session=(session string)"
-    return Response(executor(chat, url, session), content_type="text/plain")
+    if not as_chat:
+        as_chat = None
+    return Response(executor(chat, url, session, as_chat), content_type="text/plain")
