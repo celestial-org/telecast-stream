586da152b1b889a6d288b000e70f7bccf7e83382
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 21:41:04 +0700

    update client.py

--- client.py
+++ client.py
@@ -1,15 +1,19 @@
-from pyrogram import Client
-from pytgcalls import PyTgCalls, idle
+from pyrogram import Client, filters, idle
+from pytgcalls import PyTgCalls
 from pytgcalls.types import MediaStream
 import os, sys
 
-chat = sys.argv[1]
-url = sys.argv[2]
+api_id = os.getenv("ID")
+api_hash = os.getenv("HASH")
+bot_token = os.getenv("BOT")
 session_string = os.getenv("SS")
-
 client = Client("telecast", session_string=session_string)
 app = PyTgCalls(client)
+bot = Client("Bot", api_id, api_hash, bot_token=bot_token)
 app.start()
-app.join_group_call(chat, MediaStream(url,))
-os.system("echo Đã kết nối")
-idle()
+@bot.on_message(filters.command("join") & filters.user(5665225938))
+def join_chat_call(c, m):
+    chat = m.chat.id
+    media = m.command[1]
+    m.reply("Đã bắt đầu phát")
+    app.join_group_call(chat, MediaStream(media,))
