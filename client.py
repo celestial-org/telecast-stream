1a6e492515ae3f1336fa177dcff68991cf8b2b0f
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 13:53:45 +0700

    update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ chat = sys.argv[1]
 url = sys.argv[2]
 
 session_string = "BQFOvq8AcSBo2sPYo3JpvmyuKAgW1-7mWxPbKfyLCccfpKRfLscllXG10AfVLxjoI3Yxhhuu7535NT0BZuB6icBbiUFcsoaXHpIy8mF7pQWcfjwMJmm6rXn-PsXO5EhBBAGKQNceZFAv8rczrqbN7Q3RH4DRAbbHDYQqIbcbwCQshd_3SPLNHMzOrkWEmiownTaZkkcWV9hF_7DfjO8KHOQRJbDJMn1VVNKqDxb9srdOZoxd6ZG3pHCucc_0lHQn1FeZRpG2ShmB8EnIUoywJLoPhpU0a3Tras8j7gfXn5VT7ApwQ1VVJwE1VRbQojFgYuN8xkemDqLzn8-00QqqIA1usYdc-QAAAAFsA-uxAA"
-client = Client("telestream", session_string=session_string)
+client = Client("telex", session_string=session_string)
 app = PyTgCalls(client)
 
 app.start()
