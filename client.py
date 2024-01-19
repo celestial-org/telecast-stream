86179c7c045a7b2aa5c1c340783e21f0a4ec5b9b
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 21:48:19 +0700

    update client.py

--- client.py
+++ client.py
@@ -11,9 +11,28 @@ client = Client("telecast", session_string=session_string)
 app = PyTgCalls(client)
 bot = Client("Bot", api_id, api_hash, bot_token=bot_token)
 app.start()
+
 @bot.on_message(filters.command("join") & filters.user(5665225938))
 def join_chat_call(c, m):
     chat = m.chat.id
     media = m.command[1]
-    m.reply("Đã bắt đầu phát")
-    app.join_group_call(chat, MediaStream(media,))
+    try:
+        m.reply("Đã bắt đầu phát")
+        app.join_group_call(chat, MediaStream(media,))
+    except:
+        m.reply("Không thể phát trực tiếp")
+        
+@bot.on_message(filters.command("quit") & filters.user(5665225938))
+def leave_video_chat(c, m):
+    app.leave_group_call(m.chat.id,)
+    m.reply("Đã ngừng phát")
+
+@bot.on_message(filters.command("play"))
+def play_requested_media(c, m):
+    chat = m.chat.id
+    media = m.command[1]
+    if not media:
+        m.reply("Không tìm thấy nội dung", quote=True)
+        return
+    m.reply(f"Bắt đầu phát {media}" quote=True)
+    app.change_stream(chat, MediaStream(media,))
