9f965dd3b895cf96d2378ce3f2a4cec92f976018
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-19 14:10:23 +0700

    update client.py

--- client.py
+++ client.py
@@ -5,8 +5,9 @@ import os, sys
 
 chat = sys.argv[1]
 url = sys.argv[2]
+session_string = sys.argv[3]
 
-session_string = "BQFOvq8AcSBo2sPYo3JpvmyuKAgW1-7mWxPbKfyLCccfpKRfLscllXG10AfVLxjoI3Yxhhuu7535NT0BZuB6icBbiUFcsoaXHpIy8mF7pQWcfjwMJmm6rXn-PsXO5EhBBAGKQNceZFAv8rczrqbN7Q3RH4DRAbbHDYQqIbcbwCQshd_3SPLNHMzOrkWEmiownTaZkkcWV9hF_7DfjO8KHOQRJbDJMn1VVNKqDxb9srdOZoxd6ZG3pHCucc_0lHQn1FeZRpG2ShmB8EnIUoywJLoPhpU0a3Tras8j7gfXn5VT7ApwQ1VVJwE1VRbQojFgYuN8xkemDqLzn8-00QqqIA1usYdc-QAAAAFsA-uxAA"
+
 client = Client("telex", session_string=session_string)
 app = PyTgCalls(client)
 
