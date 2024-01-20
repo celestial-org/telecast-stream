2253083a63ef40abe2f21cbe2e5940b85f00bc05
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-20 10:33:22 +0700

    update main.py

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ def telestream__():
     
 @app.route("/content.mp4")
 def stream_content_channel():
-    with Client("Bot", api_id, api_hash, bot_token=bot_token, in_memory=True) as bot:
-        def gen():
-            for i in range(1, 2000):
-                try:
-                    msg = bot.get_messages("contentdownload", message_ids=i)
-                    for chunk in bot.stream_media(msg):
-                        yield chunk
-                except:
-                    continue
-        return Response(gen(), mimetype="video/mp4")
+    bot = Client("Bot", api_id, api_hash, bot_token=bot_token, in_memory=True)
+    bot.start()
+    def gen():
+        for i in range(1, 2000):
+            try:
+                msg = bot.get_messages("contentdownload", message_ids=i)
+                for chunk in bot.stream_media(msg):
+                    yield chunk
+            except:
+                continue
+    return Response(gen(), mimetype="video/mp4")
                  
