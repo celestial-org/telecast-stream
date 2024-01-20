028de373226cbc0da895eb0f753539bf05258d11
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-20 09:17:53 +0700

    update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pyrogram import Client, Response
 from init import api_id, api_hash, bot_token
 
 app = Flask("__telestream__")
+bot = Client("Bot", api_id, api_hash, bot_token=bot_token, in_memory=True)
+bot.start()
 
 @app.route("/")
 def telestream__():
@@ -10,14 +12,13 @@ def telestream__():
     
 @app.route("/content.mp4")
 def stream_content_channel(c, m):
-    with Client("Bot", api_id, api_hash, bot_token=bot_token, in_memory=True) as bot:
-        def gen():
-            for i in ran(0, 2000):
-                try:
-                    msg = bot.get_messages("contentdownload", i)
-                    for chunk in bot.stream_media(msg):
-                        yield chunk
-                except:
-                    continue
+    def gen():
+        for i in ran(0, 2000):
+            try:
+                msg = bot.get_messages("contentdownload", i)
+                for chunk in bot.stream_media(msg):
+                    yield chunk
+            except:
+                continue
     return Response(gen(), mimetype="video/mp4")
                  
