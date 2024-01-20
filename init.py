e40bcd391579b4d1157188499d5e4f878d7f7f3c
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-20 10:53:10 +0700

    update init.py

--- init.py
+++ init.py
@@ -6,4 +6,5 @@ db = deta.Base("telegram-sessions")
 api_id = db.get("API_ID")["value"]
 api_hash = db.get("API_HASH")["value"]
 bot_token = db.get("TD_TOKEN")["value"]
+cd_token = db.get("CD_TOKEN")["value"]
 session = db.get("ContentCast")["value"]
