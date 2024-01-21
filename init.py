1969e863804d9e6ec277d38b7005461f42e4bb59
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 18:43:59 +0700

    update init.py

--- init.py
+++ init.py
@@ -3,8 +3,8 @@ import os
 
 deta = Deta(os.getenv("DETA"))
 db = deta.Base("tokens")
-api_id = db.get("api_id")["value"]
-api_hash = db.get("api_hash")["value"]
-bot_token = db.get("td_token")["value"]
-cd_token = db.get("cd_token")["value"]
-session = db.get("telecast")["value"]
+api_id = db.get("api")["id"]
+api_hash = db.get("api")["hash"]
+bot_token = db.get("bot")["td"]
+cd_token = db.get("bot")["cd"]
+session = db.get("telecast")["session"]
