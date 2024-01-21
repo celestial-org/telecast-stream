eb6ad034c19370190bc279805b63a248825e6008
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 18:28:26 +0700

    update init.py

--- init.py
+++ init.py
@@ -2,9 +2,9 @@ from deta import Deta
 import os
 
 deta = Deta(os.getenv("DETA"))
-db = deta.Base("telegram-sessions")
-api_id = db.get("API_ID")["value"]
-api_hash = db.get("API_HASH")["value"]
-bot_token = db.get("TD_TOKEN")["value"]
-cd_token = db.get("CD_TOKEN")["value"]
-session = db.get("ContentCast")["value"]
+db = deta.Base("tokens")
+api_id = db.get("api_id")["value"]
+api_hash = db.get("api_hash")["value"]
+bot_token = db.get("td_token")["value"]
+cd_token = db.get("cd_token")["value"]
+session = db.get("telecast")["value"]
