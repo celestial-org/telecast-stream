99dfa7a98a44e9c243c647d4b7639afbc52b82dc
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 13:16:38 +0700

    update commands/Common.py

--- commands/Common.py
+++ commands/Common.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import os
+from api import get_video, get_audio, ttlive
+from app import join, play, leave
+from more_itertools import chunked
+import os, shelve
+
+db = shelve.open("channels.db")
 
 @Client.on_message(filters.command("setting"))
 def set_stream_quality(c, m):
@@ -18,3 +23,20 @@ def set_stream_quality(c, m):
     vq = os.getenv("VIDEO_QUAL")
     ffmpeg = on.getenv("FFMPEG")
     m.reply(f"Thông tin cài đặt:\nAudio: `{aq}`\nVideo: `{vq}`\nffmpeg: `{ffmpeg}`")
+    
+@bot.on_message(filters.command("join"))
+def join_chat_call(c, m):
+    chat = m.chat.id
+    try:
+        allkeys = list(db.keys())
+        chunked_keys = list(chunked(allkeys, 3))
+        playlist = []
+        for chunk in chunked_keys:
+            row_buttons = [InlineKeyboardButton(pre, callback_data=pre) for pre in chunk]
+            playlist.appen
+        m.reply(f"Đã bắt đầu phát sóng", reply_markup=InlineKeyboardMarkup(playlist))
+        join(chat)
+    except Exception as e:
+        print(e)
+        m.reply("Có vấn đề xảy ra! Không thể mở trình phát")
+    m.delete()
