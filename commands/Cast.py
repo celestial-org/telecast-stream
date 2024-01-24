db5bc31e146b12a49030aa1b9580718de29d5553
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 13:42:27 +0700

    update commands/Cast.py

---
+++ commands/Cast.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from api import get_video, get_audio, ttlive
+from app import join, play, leave
+from util import albums, album, add_media, del_media
+
+
+@Client.on_message(filters.command("join"))
+def join_chat_call(c, m):
+    chat = m.chat.id
+    try:
+        m.reply(f"Đã bắt đầu phát sóng", reply_markup=albums())
+        join(chat)
+    except Exception as e:
+        print(e)
+        m.reply("Có vấn đề xảy ra! Không thể mở trình phát")
+    m.delete()
