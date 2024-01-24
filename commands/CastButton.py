7bc5e09388a63eda352d7936297bf5db828bff2e
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 14:54:59 +0700

    update commands/CastButton.py

---
+++ commands/CastButton.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from util import albums, album, get_media
+from app import play
+import json
+
+@Client.on_callback_query()
+def on_callback_query_handle(c, q):
+    data = q.data
+    m = q.message
+    if data == "albums-button":
+        m.edit_reply_markup(albums())
+    if data.endswith(".album"):
+        m.edit_reply_markup(album(data))
+    else:
+        try:
+            data = json.loads(data)
+            chat = data["chat"]
+            media = data["media"]
+            q.answer(f"Bắt đầu phát {media}")
+            play(chat, media)
+        except:
+            q.answer("Không tìm thấy tài nguyên")
