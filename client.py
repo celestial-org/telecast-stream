d55e81ae08f654b6ee8f267cf9d6e6d378ee4a4b
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 15:06:45 +0700

    update client.py

--- client.py
+++ client.py
@@ -1,16 +1,6 @@
-from pyrogram import Client, filters, idle
-from pytgcalls import PyTgCalls
-from pytgcalls.types import MediaStream, AudioParameters, VideoParameters, AudioQuality, VideoQuality
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import os, sys
-from init import api_id, api_hash, bot_token, session
-from custom import on_channel
-from api import get_video, get_audio, ttlive
+from pyrogram import Client
+from init import api_id, api_hash, bot_token
 from app import app
-from more_itertools import chunked
-import shelve
-import time
-import json
 
 bot = Client("Bot", api_id, api_hash, bot_token=bot_token, in_memory=True)
 app.start()
