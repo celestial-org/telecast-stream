d0a58aecb5ce7f3ad9f4545d774cd77f542bd028
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 15:07:18 +0700

    update client.py

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ from pyrogram import Client
 from init import api_id, api_hash, bot_token
 from app import app
 
-bot = Client("Bot", api_id, api_hash, bot_token=bot_token, in_memory=True)
+bot = Client("Bot", api_id, api_hash, bot_token=bot_token, plugins=dict(root="commands"), in_memory=True)
 app.start()
 bot.run()
