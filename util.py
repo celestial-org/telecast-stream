10610e26d894928ba33ba8108a05d77be8e774fe
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 13:37:09 +0700

    update util.py

--- util.py
+++ util.py
@@ -4,14 +4,30 @@ import os, shelve
 
 db = shelve.open("channels.db")
 
-def 
-
-def playlist():
-    db = shelve.open("album/channels.db")
-    allkeys = list(db.keys())
-    chunked_keys = list(chunked(allkeys, 3))
-    playlist = []
+def albums():
+    albums_list = os.listdir("album")
+    chunked_keys = list(chunked(albums_list, 1))
+    albums = []
     for chunk in chunked_keys:
         row_buttons = [InlineKeyboardButton(pre, callback_data=pre) for pre in chunk]
-        playlist.append(row_buttons)
-    return InlineKeyboardMarkup(playlist)
+        albums.append(row_buttons)
+    return InlineKeyboardMarkup(album)
+
+def album(name):
+    db = shelve.open(f"album/Bộ sưu tập của {name}.pl")
+    allkeys = list(db.keys())
+    chunked_keys = list(chunked(allkeys, 3))
+    album = []
+    for chunk in chunked_keys:
+        row_buttons = [InlineKeyboardButton(pre, callback_data=pre) for pre in chunk]
+        album.append(row_buttons)
+    return InlineKeyboardMarkup(album)
+    
+def add_media(name, media):
+    db = shelve.open(f"album/Bộ sưu tập của {name}.pl")
+    db[media[0]]=media[1]
+    
+def del_media(name, media):
+    db = shelve.open(f"album/Bộ sưu tập của {name}.pl")
+    del db[media]
+    
