e8f94f4eb2d3e3ffd14a6eede04aa9e995cf3794
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 13:07:37 +0700

    update commands/Common.py

---
+++ commands/Common.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+
+@Client.on_message(filters.command("setting"))
+def set_stream_quality(c, m):
+    try:
+        for att in m.command:
+            if att.startswith("audio"):
+                os.environ["AUDIO_QUAL"] = att.split("=")[1]
+            if att.startswith("video"):
+                os.environ["VIDEO_QUAL"] = att.split("=")[1]
+            if att.startswith("ffmpeg"):
+                os.environ["FFMPEG"] = att.split("=")[1]
+    except:
+        pass
+    aq = os.getenv("AUDIO_QUAL")
+    vq = os.getenv("VIDEO_QUAL")
+    ffmpeg = on.getenv("FFMPEG")
+    m.reply(f"Thông tin cài đặt:\nAudio: `{aq}`\nVideo: `{vq}`\nffmpeg: `{ffmpeg}`")
