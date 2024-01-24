cfc22b0bf8a497b6663eae89adf09a293d4520f9
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-24 15:58:46 +0700

    update util.py

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ def albums():
     chunked_keys = list(chunked(albums_list, 1))
     albums = []
     for chunk in chunked_keys:
-        row_buttons = [InlineKeyboardButton(pre, callback_data=pre) for pre in chunk]
+        row_buttons = [InlineKeyboardButton(pre, callback_data=f"album:{pre}") for pre in chunk]
         albums.append(row_buttons)
     return InlineKeyboardMarkup(albums)
 
