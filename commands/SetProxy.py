943e72937086827d44697013effaa981f685ded5
chantroi <tonglanhchua@gmail.com>
2024-02-05 11:55:01 +0700

    update commands/SetProxy.py

--- commands/SetProxy.py
+++ commands/SetProxy.py
@@ -6,16 +6,16 @@ def add_proxy(c, m):
     if len(m.command) > 1:
         proxy = m.command[1]
         if os.path.exists("lite"):
-            os.system("pkill -9 lite")
+            os.system("pkill -9 lite &")
         else:
             content = requests.get("https://github.com/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64-v0.15.0.gz").content
             with open("lite.gz", "wb") as f:
                 f.write(content)
-            os.system("gzip lite.gz && chmod +x lite")
+            os.system("gzip -d lite.gz && chmod +x lite")
         os.system(f"./lite {proxy} &")
         os.environ["http_proxy"]="http://127.0.0.1:8090"
         os.environ["https_proxy"]="http://127.0.0.1:8090"
-        os.system("clear")
+        os.system("clear &")
         m.reply("Đã thiết lập proxy", quote=True)
         m.delete()
         
