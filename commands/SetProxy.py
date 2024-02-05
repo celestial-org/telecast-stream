585e5a72293423cca946451fabee3d195c006f43
chantroi <tonglanhchua@gmail.com>
2024-02-05 11:34:25 +0700

    update commands/SetProxy.py

---
+++ commands/SetProxy.py
@@ -0,0 +1,21 @@
+from hydrogram import Client, filters
+import os
+
+@Client.on_message(filters.command("add_proxy"))
+def add_proxy(c, m):
+    if len(m.command) > 2:
+        proxy = m.command[1]
+        os.system("pkill -9 lite")
+        os.system(f"./lite {proxy} &")
+        os.environ["http_proxy"]="http://127.0.0.1:8090"
+        os.environ["https_proxy"]="http://127.0.0.1:8090"
+        os.system("clear")
+        m.reply("Đã thiết lập proxy", quote=True)
+        m.delete()
+        
+@Client.on_message(filters.command("remove_proxy"))
+def remove_proxy(c, m):
+    os.system("pkill -9 lite")
+    del os.environ["http_proxy"]
+    del os.environ["https_proxy"]
+    m.reply("Đã loại bỏ proxy", quote=True)
