89626b4acaafce63dfa4436abd89e043aa4d7f58
chantroi <tonglanhchua@gmail.com>
2024-02-09 10:19:51 +0700

    update plugins/TeleCast.py

--- plugins/TeleCast.py
+++ plugins/TeleCast.py
@@ -1,5 +1,6 @@
 from hydrogram import Client, filters
 from app import Cast
+import os
 
 @Client.on_message(filters.command("join"))
 def join_chat_call(c, m):
@@ -16,3 +17,17 @@ def leave_chat_call(c, m):
     Cast(m.chat.id).end()
     m.reply("--**Chương trình đã kết thúc**--")
     m.delete()
+    
+@Client.on_message(filters.command("pause"))
+def pause_cast(c, m):
+    if not os.getenv(str(m.chat.id)):
+        Cast(m.chat.id).pause()
+        st = m.reply("Đang tạm dừng", quote=True)
+        os.environ[str(m.chat.id)] = str(st.id)
+  
+@Client.on_message(filters.command("resume"))
+def resume_cast(c, m):
+    if os.getenv(str(m.chat.id)):
+        Cast(m.chat.id).resume()
+        mid = int(os.getenv(str(m.chat.id)))
+        mid.delete()
