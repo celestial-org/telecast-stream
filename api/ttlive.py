da823b2e888939b5543ca148a73fd68e955865bd
chantroi <tonglanhchua@gmail.com>
2024-02-09 09:43:13 +0700

    update api/ttlive.py

--- api/ttlive.py
+++ api/ttlive.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import unquote
 import json
 
-def ttlive(url):
+def ttl(url):
     response = requests.get(url, stream=True, headers={"user-agent": "Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.98 Mobile Safari/537.36"})
     soup = BeautifulSoup(response.text, 'html.parser')
     state = soup.find('script', id='SIGI_STATE')
