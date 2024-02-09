8b5c2a98ecf24d51e1c947a8fd2702696207353e
chantroi <tonglanhchua@gmail.com>
2024-02-09 09:24:42 +0700

    update plugins/TeleCast.py

--- plugins/TeleCast.py
+++ plugins/TeleCast.py
@@ -1,18 +1,18 @@
 from hydrogram import Client, filters
-from app import Caller
+from app import Cast
 
 @Client.on_message(filters.command("join"))
 def join_chat_call(c, m):
     chat = m.chat.id
-    status = Caller(chat).join()
+    status = Cast(chat).join()
     if status:
         m.reply("--**Chương trình đã bắt đầu**--")
         m.delete()
     else:
         m.reply("Không thể bắt đầu phát sóng", quote=True)
         
-@Client.on_message(filters.command("quit") & filters.create(on_channel))
+@Client.on_message(filters.command("quit"))
 def leave_chat_call(c, m):
-    leave(m.chat.id)
+    Cast(m.chat.id).end()
     m.reply("--**Chương trình đã kết thúc**--")
     m.delete()
