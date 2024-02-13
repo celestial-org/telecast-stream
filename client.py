71c9214fa2f858e05a3594b347072b77f3d3ed31
chantroi <tonglanhchua@gmail.com>
2024-02-13 14:26:15 +0700

    update client.py

--- client.py
+++ client.py
@@ -1,8 +1,4 @@
-from hydrogram import Client
-from deta import Deta 
-from app import app
-from environ import api_id, api_hash, bot_token
+from app import client, app
 
-bot = Client("Bot", api_id, api_hash, bot_token=bot_token, plugins=dict(root="plugins"), in_memory=True)
 app.start()
-bot.run()
+client.run()
