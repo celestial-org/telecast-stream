4e4a1fe3b1b8a4503470ff657046bca4252fb426
chantroi <tonglanhchua@gmail.com>
2024-02-16 10:57:19 +0700

    update plugins/Basic.py

--- plugins/Basic.py
+++ plugins/Basic.py
@@ -2,5 +2,6 @@ from hydrogram import Client, filters
 
 @Client.on_message(group=2)
 def react_all(c, m):
-    c.read_chat_history(m.chat.id)
-    m.react(emoji="❤️", big=True)
+    if not m.from_user.is_bot:
+        c.read_chat_history(m.chat.id)
+        m.react(emoji="❤️", big=True)
