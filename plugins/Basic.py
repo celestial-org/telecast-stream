2cbb3e6207d0d4bc27f5d341b42d5de522574bf9
chantroi <tonglanhchua@gmail.com>
2024-02-16 11:02:02 +0700

    update plugins/Basic.py

--- plugins/Basic.py
+++ plugins/Basic.py
@@ -1,7 +1,11 @@
 from hydrogram import Client, filters
+import random
+
+emojis = ["👍", "❤️", "😍", "🔥", "🥰", "👏", "💔", "💘", "🆒"]
 
 @Client.on_message(group=2)
 def react_all(c, m):
+    emo = random.choice(emojis)
     if not m.from_user.is_bot:
         c.read_chat_history(m.chat.id)
-        m.react(emoji="❤️", big=True)
+        m.react(emoji=emo, big=True)
