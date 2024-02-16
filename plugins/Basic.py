63b48eee7d68684a339ee14ddb30a33ef9e7221f
chantroi <tonglanhchua@gmail.com>
2024-02-16 17:31:08 +0700

    update plugins/Basic.py

--- plugins/Basic.py
+++ plugins/Basic.py
@@ -4,7 +4,7 @@ import random
 #emojis = ["👍", "❤️", "😍", "🔥", "🥰", "👏", "💔", "💘", "🆒", "💯", "🌚", "🌭", "💋", "🍌", "🍓", "🕊️"]
 
 @Client.on_message(group=2)
-def react_all(c, m):
+def read_all(c, m):
    # emo = random.choice(emojis)
     if not m.from_user.is_bot:
         c.read_chat_history(m.chat.id)
