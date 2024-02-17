7dc8bc45ebd6ec5bdf214cd665025a8cabbc83e8
chantroi <tonglanhchua@gmail.com>
2024-02-17 19:19:01 +0700

    update environ.py

--- environ.py
+++ environ.py
@@ -1,10 +1,6 @@
-from deta import Deta 
+import requests
 import os
 
-deta = Deta(os.getenv("DETA"))
-db = deta.Base("tokens")
-api_id = db.get("api")["id"]
-api_hash = db.get("api")["hash"]
-bot_token = db.get("bot")["td"]
-cd_token = db.get("bot")["cd"]
-session = db.get("telecast")["ss"]
+secret = os.getenv("SECRET")
+r = requests.get(secret)
+session = r.text
