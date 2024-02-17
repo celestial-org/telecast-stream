7b09dfa9f878d930307f85fb4564334f4543748c
chantroi <tonglanhchua@gmail.com>
2024-02-17 19:23:42 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-export FFMPEG="" VIDEO_QUAL=1920,1080,60 AUDIO_QUAL=20000,2
+
 echo "Bắt đầu khởi động..."
 python -u client.py &
 gunicorn -b 0.0.0.0:8080 --timeout 100000 --log-level critical main:app
